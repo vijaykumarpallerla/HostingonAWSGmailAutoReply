dd3fce749106cd366fa1176d63ec8c3567ec7300
vijaykumarpallerla <207540509+vijaykumarpallerla@users.noreply.github.com>
2025-12-28 10:31:35 +0530

    Allow all hosts

--- gmail_auto_reply/settings.py
+++ gmail_auto_reply/settings.py
@@ -38,19 +38,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-jit4840ra0av7ksoq!7&5-+viz+n80r3)-&yqfi7=sp$#7j(5e')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-
-# Allow OAuth over http://127.0.0.1 or http://localhost during local development only
-if DEBUG:
-    os.environ.setdefault('OAUTHLIB_INSECURE_TRANSPORT', '1')
-
-
 # Application definition
 
 INSTALLED_APPS = [
