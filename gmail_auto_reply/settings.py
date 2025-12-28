f95fbc34ba62a6ca673c2b86d340c229a763d7b0
vijaykumarpallerla <207540509+vijaykumarpallerla@users.noreply.github.com>
2025-12-28 10:35:18 +0530

    Restore settings

--- gmail_auto_reply/settings.py
+++ gmail_auto_reply/settings.py
@@ -38,6 +38,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-jit4840ra0av7ksoq!7&5-+viz+n80r3)-&yqfi7=sp$#7j(5e')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
 # Application definition
 
 INSTALLED_APPS = [
